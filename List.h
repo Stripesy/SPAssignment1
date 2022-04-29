368a9d89244c465601cab140bbc7438b3fb72cc4
Stripesy <nicholas.vecchio02@gmail.com>
2022-04-29 22:46:29 +1000

    Added sort function.

--- List.h
+++ List.h
@@ -8,11 +8,45 @@ typedef struct iorb {
 } IORB, *IORBPtr;
 
 void sortList(IORB **head,int(*prio)(int)) {
+
+    int largestValue;
+    char largestFiller[100];
+    IORBPtr current = *head;
+    IORBPtr largestBlock = NULL;
+    IORBPtr next = NULL;
+
+    while(current != NULL) {
+        largestBlock = current;
+        next = current->link;
+
+        while(next != NULL) {
+            if(((prio)(next->base_pri) > (prio)(largestBlock->base_pri))) {
+                largestBlock = next;
+            }
+            next = next->link;
+        }
+    
+    if(largestBlock != current) {
+        largestValue = (prio)(largestBlock->base_pri);
+        strcpy(largestFiller, largestBlock->filler);
+
+        largestBlock->base_pri = current->base_pri;
+        strcpy(largestBlock->filler, current->filler);
+
+        current->base_pri = largestValue;
+        strcpy(current->filler, largestBlock->filler);
+        
+    }
+    current = current->link;
+    }
 }
+
+    
+
 
 int priComp(int basepriority) {
 
-    return basepriority%7; //to be properly implemented. Currently just a filler.
+    return basepriority; //to be properly implemented. Currently just a filler.
 
 }
 
@@ -55,7 +89,7 @@ void printList(IORBPtr head, int (*prio)(int)) {
     while(current != NULL) {
         printf("Block Description: %s, ", current->filler);
         printf("Base Priority: %d, ", current->base_pri);
-        printf("Priority: %d, \n",(prio)(current->base_pri));
+        printf("Priority: %d \n",(prio)(current->base_pri));
         current = current->link;
     }
 
--- main.c
+++ main.c
@@ -6,17 +6,35 @@
 #include "List.h"
 
 void main() {
-    
+
     srand(time(0));
 
     int size = 9;
 
     IORBPtr head;
 
-    buildList(&head,size);
-
     int (*priority) (int)=priComp;
 
-    printList(head,priority);
+    int userInput = -1;
+
+    while(userInput != 0) {
+
+        printf("\nPlease enter your choice: \n 0: Exit\n 1: Build List\n 2: Sort list\n 3: Display list\n");
+
+        scanf("%d", &userInput);
+
+        switch(userInput) {
+        
+            case 1:
+            buildList(&head,size);
+            break;
+            case 2:
+            sortList(&head,priority);
+            break;
+            case 3:
+            printList(head,priority);
+            break;
+        }
+    }
 
 }
